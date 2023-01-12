68ad98d92c9c24d14a6307568dc2b3ed7597b9e6
mozoroff <mozoshima@gmail.com>
2023-01-12 22:43:31 +0900

    Initial Commit

---
+++ Shared/Mini_Project01App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_Project01App.swift
+//  Shared
+//
+//  Created by kensho on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct Mini_Project01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
